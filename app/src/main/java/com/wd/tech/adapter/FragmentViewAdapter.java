9a1fb04238f5c12e0978232ac17e8317217630f1
yuzhenchaoIT <45099361+yuzhenchaoIT@users.noreply.github.com>
2019-02-20 10:39:15 +0800

    信息界面

---
+++ app/src/main/java/com/wd/tech/adapter/FragmentViewAdapter.java
@@ -0,0 +1,26 @@
+package com.wd.tech.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class FragmentViewAdapter extends FragmentPagerAdapter {
+    private List<Fragment> list;
+
+    public FragmentViewAdapter(FragmentManager fm, List<Fragment> list) {
+        super(fm);
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
---
+++ app/src/main/java/com/wd/tech/frag/FragOneContact.java
@@ -0,0 +1,22 @@
+package com.wd.tech.frag;
+
+import com.wd.tech.R;
+import com.wd.tech.core.WDFragment;
+
+
+public class FragOneContact extends WDFragment {
+    @Override
+    public String getPageName() {
+        return null;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.frag_one_contact;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
