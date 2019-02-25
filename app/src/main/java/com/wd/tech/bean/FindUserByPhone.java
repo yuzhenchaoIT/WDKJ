5cf540da961774f3555c20082314c21e8bbf53fe
yuzhenchaoIT <45099361+yuzhenchaoIT@users.noreply.github.com>
2019-02-25 08:01:26 +0800

    分组信息

---
+++ app/src/main/java/com/wd/tech/bean/FindUserByPhone.java
@@ -0,0 +1,98 @@
+package com.wd.tech.bean;
+
+public class FindUserByPhone {
+
+    /**
+     * headPic : http://mobile.bwstudent.com/images/tech/default/tech.jpg
+     * integral : 0
+     * nickName : 可口可乐
+     * phone : 16619785294
+     * sex : 1
+     * userId : 41
+     * userName : JnA5Rn16619785294
+     * whetherFaceId : 2
+     * whetherVip : 2
+     */
+
+    private String headPic;
+    private int integral;
+    private String nickName;
+    private String phone;
+    private int sex;
+    private int userId;
+    private String userName;
+    private int whetherFaceId;
+    private int whetherVip;
+
+    public String getHeadPic() {
+        return headPic;
+    }
+
+    public void setHeadPic(String headPic) {
+        this.headPic = headPic;
+    }
+
+    public int getIntegral() {
+        return integral;
+    }
+
+    public void setIntegral(int integral) {
+        this.integral = integral;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getWhetherFaceId() {
+        return whetherFaceId;
+    }
+
+    public void setWhetherFaceId(int whetherFaceId) {
+        this.whetherFaceId = whetherFaceId;
+    }
+
+    public int getWhetherVip() {
+        return whetherVip;
+    }
+
+    public void setWhetherVip(int whetherVip) {
+        this.whetherVip = whetherVip;
+    }
+}
