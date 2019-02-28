7ca8a8831c865b0cb1b9017fc7dcb0001c5567f4
wzl844736302 <844736302@qq.com>
2019-02-28 11:21:20 +0800

    请求列表111111

--- app/src/main/java/com/wd/tech/core/http/IRequest.java
+++ app/src/main/java/com/wd/tech/core/http/IRequest.java
@@ -19,6 +19,7 @@ import com.wd.tech.bean.MyPost;
 import com.wd.tech.bean.NoticeList;
 import com.wd.tech.bean.QueryUser;
 import com.wd.tech.bean.Result;
+import com.wd.tech.bean.TaskList;
 import com.wd.tech.bean.User;
 import com.wd.tech.bean.UserPost;
 import com.wd.tech.bean.UserIntegral;
@@ -510,4 +511,64 @@ public interface IRequest {
             @Header("userId") int userId,
             @Header("sessionId") String sessionId
     );
+
+    /**
+     * 转移好友到其他分组
+     * @param userId
+     * @param sessionId
+     * @param newGroupId
+     * @param friendUid
+     * @return
+     */
+    @PUT("chat/verify/v1/transferFriendGroup")
+    @FormUrlEncoded
+    Observable<Result> transferFriendGroup(@Header("userId") int userId,
+                                           @Header("sessionId") String sessionId,
+                                           @Field("newGroupId")int newGroupId,
+                                           @Field("friendUid")int friendUid);
+
+    @GET("chaterify1/findFriendNoticePageList")
+    Observable<Result<List<FriendNoticePageList>>> friendNoticePageList(
+            @Header("userId") int userId,
+            @Header("sessionId") String sessionId,
+            @Query("page") int page,
+            @Query("count") int count
+    );
+    @PUT("chaterify1/reviewFriendApply")
+    Observable<Result> reviewFriend(
+            @Header("userId") int userId,
+            @Header("sessionId") String sessionId,
+            @Query("noticeId") int noticeId,
+            @Query("flag") int flag
+    );
+    /**
+     * 删除好友
+     * @param userId
+     * @param sessionId
+     * @param friendUid
+     * @return
+     */
+    @DELETE("chaterify1/deleteFriendRelation")
+    Observable<Result> deleteFriendRelation(@Header("userId") int userId,
+                                            @Header("sessionId")String sessionId,
+                                            @Query("friendUid") int friendUid);
+
+    /**
+     * 查看别人的社区
+     * @param userId
+     * @param sessionId
+     * @param fromUid
+     * @param page
+     * @param count
+     * @return
+     */
+    @GET("community/verify/v1/findUserPostById")
+    Observable<Result<List<UserPost>>> findUserPostById(
+            @Header("userId") int userId,
+            @Header("sessionId") String sessionId,
+            @Query("fromUid") int fromUid,
+            @Query("page") int page,
+            @Query("count") int count);
+
+
 }
